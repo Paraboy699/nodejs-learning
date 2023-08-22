5558aa80e80fb04c053dc2c9229b3fab1e2d38a8
Pukar Singh <spukar133@gmail.com>
2023-08-22 09:13:58 +0545

    Learn about npm

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require("http");
 const myModules = require("./myCustomModule");
+const upperCase = require("upper-case");
 
 const server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
   const currentDate = myModules.getCurrentDate();
+  const myName = upperCase.upperCase("Pukar");
+
+  res.write("<h1> Hello World " + myName + "</h1>");
   res.end(`Current Date: ${currentDate}`);
 });
 
